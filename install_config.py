6aa5a727d06602626012cfb6e2bc83aacd58df4b
Steen Hegelund <steen.hegelund@microsemi.com>
2017-12-18 20:58:54 +0100

    Separated installation folder and filename

--- install_config.py
+++ install_config.py
@@ -7,18 +7,22 @@ import readline
 import shutil
 import enum
 
-class UserChoice(enum.Enum):
-    SkipFile = 0
-    OverWriteFile = 1
-    InstallFile = 2
 
+class UserChoice(enum.Enum):
+    SkipFile = '...'
+    OverWriteFile = '=>*'
+    InstallFile = '==>'
 
 
 class InstallableElement:
-    def __init__(self, name, installed_path):
+    def __init__(self, name, path, installed_name=None, level=1, indent=3):
         self.name = name
         self.source_path = os.path.abspath(name)
-        self.installed_path = os.path.expanduser(installed_path)
+        self.path = os.path.expanduser(path)
+        self.installed_name = installed_name if installed_name is not None else name
+        self.installed_path = os.path.join(self.path, self.installed_name)
+        self.prefix = ' ' * level * indent
+        self.user_choice = UserChoice.SkipFile
 
     def exists(self):
         return os.path.exists(self.installed_path)
@@ -36,37 +40,40 @@ class InstallableElement:
     def user_action(self):
         if self.exists():
             if self.overwrite_existing_prompt() == 'y':
-                return UserChoice.OverWriteFile
-            print('Skipping: {}'.format(self.installed_path))
-            return UserChoice.SkipFile
-        if self.install_prompt() == 'y':
-            return UserChoice.InstallFile
-        return UserChoice.SkipFile
+                self.user_choice = UserChoice.OverWriteFile
+            else:
+                print('Skipping: {}'.format(self.installed_path))
+                self.user_choice = UserChoice.SkipFile
+        elif self.install_prompt() == 'y':
+            self.user_choice = UserChoice.InstallFile
+        else:
+            self.user_choice = UserChoice.SkipFile
 
-    def __str__(self):
-        return '{}: {} {}'.format(self.name, self.source_path, self.installed_path)
+    def show_action(self):
+        if self.user_choice == UserChoice.SkipFile:
+            return '{}{} Skipped'.format(self.prefix, self.source_path)
+        return '{}{} {} {}'.format(self.prefix, self.source_path, self.user_choice, self.installed_path)
 
 
 class SymLinkFile(InstallableElement):
     def install(self):
-        action = self.user_action()
-        if action == UserChoice.SkipFile:
+        if self.user_choice == UserChoice.SkipFile:
             return
-        if action == UserChoice.OverWriteFile:
+        if self.user_choice == UserChoice.OverWriteFile:
             os.remove(self.installed_path)
-        print('Symlink {} to {}'.format(self.installed_path , self.source_path))
         os.symlink(self.source_path, self.installed_path)
 
     def __str__(self):
-        return '{}: {} -> {}'.format(self.name, self.source_path, self.installed_path)
+        return '{}{}'.format(self.prefix, self.installed_name)
 
 
-class FolderOfSymlinks(InstallableElement):
-    def __init__(self, name, installed_path, *elements):
-        super().__init__(name, installed_path)
+class FolderOfElements(InstallableElement):
+    def __init__(self, name, path, installed_name=None, *elements, level=1, indent=3):
+        super().__init__(name, path, installed_name, level=level, indent=indent)
+        elements = os.listdir(name) if len(elements) == 0 else elements
         self.elements = []
-        for elem_name in elements:
-            self.elements.append(SymLinkFile(elem_name, os.path.join(self.installed_path, elem_name)))
+        for element_name in elements:
+            self.elements.append(SymLinkFile(element_name, self.installed_path, element_name, level=level+1))
 
     def install(self):
         print('Nothing here yet')
@@ -74,31 +81,31 @@ class FolderOfSymlinks(InstallableElement):
         # If the folder exists, the symlinks should still be refreshed
 
     def __str__(self):
-        result = super().__str__()
+        result = '{}{}'.format(self.prefix, self.installed_path)
         for elem in self.elements:
             result += '\n' + str(elem)
         return result
 
 
-
 elements = [
-    SymLinkFile('bash_logout', '~/.bash_logout'),
-    SymLinkFile('bash_profile', '~/.bash_profile'),
-    SymLinkFile('bashrc', '~/.bashrc'),
-    SymLinkFile('gitconfig', '~/.gitconfig'),
-    SymLinkFile('gitignore', '~/.gitignore'),
-    SymLinkFile('inputrc', '~/.inputrc'),
-    SymLinkFile('profile', '~/.profile'),
-    SymLinkFile('vimrc', '~/.vimrc'),
-    SymLinkFile('Xdefaults', '~/.Xdefaults'),
-    SymLinkFile('Xmodmap', '~/.Xmodmap'),
-    FolderOfSymlinks('profile.d', '~/.profile.d', *os.listdir('profile.d')),
+    SymLinkFile('bash_logout', '~', '.bash_logout'),
+    SymLinkFile('bash_profile', '~', '.bash_profile'),
+    SymLinkFile('bashrc', '~', '.bashrc'),
+    SymLinkFile('gitconfig', '~', '.gitconfig'),
+    SymLinkFile('gitignore', '~', '.gitignore'),
+    SymLinkFile('inputrc', '~', '.inputrc'),
+    SymLinkFile('profile', '~', '.profile'),
+    SymLinkFile('vimrc', '~', '.vimrc'),
+    SymLinkFile('Xdefaults', '~', '.Xdefaults'),
+    SymLinkFile('Xmodmap', '~', '.Xmodmap'),
+    FolderOfElements('profile.d', '~', '.profile.d'),
 ]
 
 if __name__ == '__main__':
+    os.chdir('/home/shegelun/src/steen-linux-config')
     for elem in elements:
         print(elem)
-    for elem in elements:
-        elem.install()
+    # for elem in elements:
+    #     elem.install()
 
 
