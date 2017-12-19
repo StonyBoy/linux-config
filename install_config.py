c56d10036546ec89a83b4d74812d456c4f8ba7d5
Steen Hegelund <steen@hegelund-hansen.dk>
2017-12-19 22:55:40 +0100

    New installer design

--- install_config.py
+++ install_config.py
@@ -9,31 +9,27 @@ import enum
 
 
 class UserChoice(enum.Enum):
-    SkipFile = '...'
+    SkipFile = '.X.'
     OverWriteFile = '=>*'
     InstallFile = '==>'
 
 
-class InstallableElement:
-    def __init__(self, name, path, installed_name=None, level=1, indent=3):
-        self.name = name
-        self.source_path = os.path.abspath(name)
-        self.path = os.path.expanduser(path)
-        self.installed_name = installed_name if installed_name is not None else name
-        self.installed_path = os.path.join(self.path, self.installed_name)
-        self.prefix = ' ' * level * indent
+class Installer:
+    def __init__(self, src_path, dst_path):
+        self.src_path = src_path
+        self.dst_path = dst_path
         self.user_choice = UserChoice.InstallFile
 
     def exists(self):
-        return os.path.exists(self.installed_path)
+        return os.path.exists(self.src_path)
 
     def overwrite_existing_prompt(self):
-        prompt = '{} already exists - Do your want to overwrite it: (N/y) > '.format(self.installed_path)
+        prompt = '{} already exists - Do your want to overwrite it: (N/y) > '.format(self.src_path)
         response = input(prompt)
         return response.lower()
 
     def install_prompt(self):
-        prompt = 'Do your want to install {}: (Y/n) > '.format(self.installed_path)
+        prompt = 'Do your want to install {}: (Y/n) > '.format(self.src_path)
         response = input(prompt)
         return response.lower()
 
@@ -42,7 +38,7 @@ class InstallableElement:
             if self.overwrite_existing_prompt() == 'y':
                 self.user_choice = UserChoice.OverWriteFile
             else:
-                print('Skipping: {}'.format(self.installed_path))
+                print('Skipping: {}'.format(self.src_path))
                 self.user_choice = UserChoice.SkipFile
         elif self.install_prompt() == 'y':
             self.user_choice = UserChoice.InstallFile
@@ -50,13 +46,10 @@ class InstallableElement:
             self.user_choice = UserChoice.SkipFile
 
     def show_action(self):
-        if self.user_choice == UserChoice.SkipFile:
-            print('{}{} Skipped'.format(self.prefix, self.source_path))
-        else:
-            print('{}{} {} {}'.format(self.prefix, self.source_path, self.user_choice.value, self.installed_path))
+        print('{} {}'.format(self.src_path, self.user_choice.value))
 
 
-class FileLink(InstallableElement):
+class FileLink(Installer):
     def install(self):
         if self.user_choice == UserChoice.SkipFile:
             return
@@ -68,53 +61,106 @@ class FileLink(InstallableElement):
         return '{}{} : {}'.format(self.prefix, self.installed_name, self.installed_path)
 
 
-class GroupLink(InstallableElement):
-    def __init__(self, name, path, installed_name=None, level=1, indent=3):
-        self.elements = []
-        super().__init__(name, path, installed_name, level=level, indent=indent)
-        for root, folders, files in os.walk(name):
-            # print('root', root)
-            dst_path = os.path.join(self.installed_path, root.replace(name + '/', ''))
-            # print('dst_path', dst_path)
-            for file in files:
-                src_path = os.path.join(root, file)
-                self.elements.append(FileLink(src_path, dst_path, level=level + 1))
+class File:
+    def __init__(self, path, name):
+        self.path = path
+        self.name = name
 
-    def install(self):
-        print('Nothing here yet')
-        # Create folder if it does not exist.  Symlink elements into folder
-        # If the folder exists, the symlinks should still be refreshed
-
-    def show_action(self):
-        for element in self.elements:
-            element.show_action()
+    def get_fullpath(self):
+        return os.path.join(self.path, self.name)
 
     def __str__(self):
-        result = '{}{}'.format(self.prefix, self.installed_path)
+        return '{}/{}'.format(self.path, self.name)
+
+
+class Folder:
+    def __init__(self, name):
+        self.name = name
+        self.elements = []
+        self.destination = None
+        self.destination_name = name
+        for root, folders, files in os.walk(name):
+            for file in files:
+                self.elements.append(File(root, file))
+
+    def set_name(self, name):
+        self.destination_name = '.' + self.name if name == '.' else name
+
+    def set_destination(self, path):
+        self.destination = os.path.join(os.path.expanduser(path), self.destination_name)
+
+    def __str__(self):
+        result = '{}/ -> {}'.format(self.name, self.destination)
         for elem in self.elements:
             result += '\n' + str(elem)
         return result
 
 
+class FileList:
+    def __init__(self, name, *files):
+        self.name = name
+        self.elements = []
+        self.destination = None
+        self.destination_name = name
+        self.dot_rename_files = False
+        self.installers = []
+        for file in files:
+            self.elements.append(File(name, file))
+
+    def set_name(self, name):
+        self.destination_name = '.' + self.name if name == '.' else name
+
+    def set_destination(self, path):
+        self.destination = os.path.join(os.path.expanduser(path), self.destination_name)
+
+    def dot_named_files(self):
+        self.dot_rename_files = True
+
+    def prepare_install(self):
+        if self.dot_rename_files:
+            for item in self.elements:
+                self.installers.append(Installer(item.get_fullpath(), os.path.join(self.destination, '.' + item.name)))
+        else:
+            for item in self.elements:
+                self.installers.append(Installer(item.get_fullpath(), os.path.join(self.destination, item.name)))
+
+    def __str__(self):
+        result = '{}/ -> {}'.format(self.name, self.destination)
+        if self.dot_rename_files:
+            for elem in self.elements:
+                result += '\n{} -> {}/.{}'.format(elem, elem.path, elem.name)
+        else:
+            for elem in self.elements:
+                result += '\n{}'.format(elem)
+        return result
+
+
 elements = [
-    FileLink('bash_logout', '~', '.bash_logout'),
-    FileLink('bash_profile', '~', '.bash_profile'),
-    FileLink('bashrc', '~', '.bashrc'),
-    FileLink('gitconfig', '~', '.gitconfig'),
-    FileLink('gitignore', '~', '.gitignore'),
-    FileLink('inputrc', '~', '.inputrc'),
-    FileLink('profile', '~', '.profile'),
-    FileLink('vimrc', '~', '.vimrc'),
-    FileLink('Xdefaults', '~', '.Xdefaults'),
-    FileLink('Xmodmap', '~', '.Xmodmap'),
-    GroupLink('profile.d',          '~', '.profile.d'),
-    GroupLink('scripts',            '~'),
-    GroupLink('config',             '~', '.config'),
+    FileList(os.getcwd(),
+        'bash_logout',
+        'bash_profile',
+        'bashrc',
+        'gitconfig',
+        'gitignore',
+        'inputrc',
+        'profile',
+        'vimrc',
+        'Xdefaults'
+        'Xmodmap',
+        'tmux.conf'),
+    Folder('profile.d'),
+    Folder('scripts'),
+    Folder('config'),
 ]
 
 if __name__ == '__main__':
-    for elem in elements:
-        elem.show_action()
+    for item in elements[0:1]:
+        item.dot_named_files()
+    for item in elements[1:]:
+        item.set_name('.')
+        item.set_destination('~')
+    for item in elements:
+        print(item)
     # for elem in elements:
     #     elem.install()
 
