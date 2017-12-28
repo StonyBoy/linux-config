2984bf1889c6aac197b96db2e4cd4b6c39e8e24c
Steen Hegelund <steen.hegelund@microsemi.com>
2017-12-28 13:39:03 +0100

    Multimedia keys

--- profile.d/50-private.sh
+++ profile.d/50-private.sh
@@ -1,6 +1,6 @@
 # -*-sh-*- 
 # .bash_profile
-# Time-stamp: <06-dec-17 21:09
+# Time-stamp: <28-dec-17 21:09>
 # Settings for all interactive shells
 
 # Debugging
@@ -29,6 +29,11 @@ ulimit -c unlimited
 
 # Capslock as Ctrl
 # /usr/bin/setxkbmap -option "caps:ctrl_modifier"
+
+# Keyboard mapping for keyboards without multimedia keys (PrintScreen, ScrollLock, Pause)
+xmodmap -e "keycode 107 = XF86AudioLowerVolume XF86AudioLowerVolume XF86AudioLowerVolume XF86AudioLowerVolume"
+xmodmap -e "keycode 78  = XF86AudioRaiseVolume XF86AudioRaiseVolume XF86AudioRaiseVolume XF86AudioRaiseVolume"
+xmodmap -e "keycode 127 = XF86AudioPlay XF86AudioPlay XF86AudioPlay XF86AudioPlay"
 
 # Global Command aliases
 
@@ -62,6 +67,44 @@ function qtc()
     qtcreator -pid $(pgrep -of qtcreator) "$*"
 }
 
+function _complete_qtproj()
+{
+    COMPREPLY=( $(compgen -W "$(ls *.creator | sed -e 's/.creator//g')") )
+    return 0
+}
+
+function qtproj()
+{
+    python3 ~/src/python/qtcreator_project/qtcreator_webstax_project.py "$*"
+}
+complete -F _complete_qtproj qtproj
+
+function filtertext()
+{
+    if [[ $# > 0 ]]; then
+        for fn in $*; do
+            echo $1
+            if [[ -e $1 ]]; then
+                cat $1 | tr -d '\000-\011' | tr -d '\013-\037' | tr -d '\200-\377' > $1.filtered
+                mv $1.filtered $1
+            fi
+            shift
+        done
+    fi
+}
+
+function _complete_start_vnc()
+{
+    COMPREPLY=( $( compgen -W "mac pc single" -- ${COMP_WORDS[COMP_CWORD]} ) )
+    return 0
+}
+
+function show_vnc()
+{
+    echo "VNC Service: "
+    systemctl status x0vncserver | grep "Active:"
+}
+
 function start_vnc()
 {
     x0vncserver -display :0 -Geometry 1920x1200 -SecurityTypes vncauth,tlsvnc -rfbauth ~/.vnc/passwd -rfbport 5900
@@ -72,6 +115,28 @@ function show_ip()
 {
     ip addr list wan | grep 'inet ' | grep -v '127.0.0.1' | cut -d" " -f6
 }
+
+function _complete_displaymode()
+{
+    COMPREPLY=( $( compgen -W "wide single show" -- ${COMP_WORDS[COMP_CWORD]} ) )
+    return 0
+}
+
+function displaymode()
+{
+    local PRI=DVI-0
+    local SEC=HDMI-0
+    if [[ "$1" == "wide" ]]; then
+        xrandr --display :0 --screen 0 --fb 3840x1200  --output ${SEC} --mode 1920x1080 --output ${PRI} --mode 1920x1200 --left-of ${SEC} --primary
+    elif [[ "$1" == "single" ]]; then
+        xrandr --display :0 --screen 0 --fb 1920x1200  --output ${PRI} --mode 1920x1200 --primary --output ${SEC} --off
+    elif [[ "$1" == "show" ]]; then
+        xrandr --display :0 | grep " connected" | cut -d" " -f1 
+    else
+        xrandr
+    fi
+}
+complete -F _complete_displaymode displaymode dm
 
 function jointime()
 {
@@ -85,7 +150,3 @@ function leavetime()
     echo Leave: $NOW  >> ~/.worklog/log.txt
 }
 
-function gitpatchsha()
-{
-    git format-patch -M -C $1~1..$1
-}
