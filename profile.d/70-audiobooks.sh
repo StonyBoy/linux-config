d64bc10f21fb2630e64fac0550f0dab13fd329d2
Steen Hegelund <steen@hegelund-hansen.dk>
2019-03-10 22:36:45 +0100

    bash: Android emulator functions

--- profile.d/70-audiobooks.sh
+++ profile.d/70-audiobooks.sh
@@ -11,6 +11,11 @@ function delete_audiobooks()
     adb shell "cd /sdcard/Android/data/dk.ereolen/files/356yth7uj4567u34t734e56yuijt456y7uij3ret/fg8hj534t80g5h7v9085ut39-28ug-k32f94230g && rm -rf *"
 }
 
+function install_ereolen()
+{
+    adb install ~/Google\ Drive/Teknik/Udvikling/Android/eReolen_2.3.12.apk
+}
+
 function start_ereolen()
 {
     emulator @steen_ereolen
@@ -24,6 +29,7 @@ function list_androidsdk()
 function update_androidsdk()
 {
     sdkmanager platform-tools
+    sdkmanager emulator
     sdkmanager "system-images;android-28;google_apis;x86"
     sdkmanager "platforms;android-28"
 }
