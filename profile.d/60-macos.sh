387735f5653196aa4d18698456bf3f605aec48c7
Steen Hegelund <steen@hegelund-hansen.dk>
2019-03-26 22:14:35 +0100

    MacOS: brew: added sbin path, removed brew API token

--- profile.d/60-macos.sh
+++ profile.d/60-macos.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # -*-sh-*-
 
-export PATH=~/scripts:~/.local/bin:~/bin:/usr/local/bin:$PATH
+export PATH=~/scripts:~/.local/bin:~/bin:/usr/local/bin:/usr/local/sbin:$PATH
 
 # set -x
 
@@ -14,5 +14,4 @@ function brew_update()
 
 export ANDROID_SDK_ROOT=~/android-sdk
 export PATH=~/android-sdk/emulator:~/android-sdk/tools/bin:~/android-sdk/platform-tools:$PATH
-export HOMEBREW_GITHUB_API_TOKEN=ea94f01f5bd90925c1a28976725087b5c193dffa
 
