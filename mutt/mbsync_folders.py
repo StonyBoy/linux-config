60caf3ee2fca31816155d1b61e45677d35662142
Steen Hegelund <steen.hegelund@microchip.com>
2020-01-14 11:36:21 +0100

    mutt: sync folders with or without an account folder

--- mutt/mbsync_folders.py
+++ mutt/mbsync_folders.py
@@ -2,9 +2,12 @@
 import os
 import sys
 
-def get_mailbox_folder_list(top):
-    base = os.path.dirname(top) + os.sep
-    account = os.path.basename(top)
+def get_mailbox_folder_list(top, account = None):
+    base = top + os.sep
+    if account:
+        top = os.path.join(top, account) + os.sep
+    else:
+        top = top + os.sep
     result = []
     for dirpath, dirnames, filenames in os.walk(top):
         if 'cur' in dirnames:
@@ -12,8 +15,11 @@ def get_mailbox_folder_list(top):
     return result
 
 
-if len(sys.argv) > 1:
+if len(sys.argv) == 2:
     top = os.path.expanduser(sys.argv[1]).rstrip(os.sep)
     print(' '.join(get_mailbox_folder_list(top)))
+elif len(sys.argv) == 3:
+    top = os.path.expanduser(sys.argv[1]).rstrip(os.sep)
+    print(' '.join(get_mailbox_folder_list(top, sys.argv[2])))
 else:
     print('Usage: {} <mailbox account path>'.format(sys.argv[0]))
