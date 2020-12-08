2eca1381c3a5339d5339167143fba1c07bfd958b
Steen Hegelund <steen@hegelund-hansen.dk>
2020-12-08 17:31:59 +0100

    i3: rofi remote tmux, add X11 forwarding terminal

--- scripts/remote_tmux.sh
+++ scripts/remote_tmux.sh
@@ -3,10 +3,22 @@
 # Shows a list of remote session names when started without an argument
 # Can be used from rofi
 # Steen Hegelund
-# Time-Stamp: 2020-Dec-08 10:02
+# Time-Stamp: 2020-Dec-08 17:23
 
 if [ $# -gt 0 ]; then
-    coproc (alacritty --title "$1" -e ssh -t work tmux -u attach-session -t $1)
+    case "$1" in
+        "Plain SSH Terminal")
+            coproc (alacritty --title "Work SSH" -e ssh -t work)
+            ;;
+        "X11 Forwarding SSH Terminal")
+            coproc (alacritty --title "X11 FWD Work SSH" -e ssh -Y -t work)
+            ;;
+        *)
+            coproc (alacritty --title "$1" -e ssh -t work tmux -u attach-session -t $1)
+            ;;
+    esac
 else
+    echo "Plain SSH Terminal"
+    echo "X11 Forwarding SSH Terminal"
     ssh work tmux list-sessions | sed -E 's/:.*//'
 fi
