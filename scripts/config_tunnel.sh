cb57c068eaa933dae1891c80c076a690b93a0229
Steen Hegelund <steen@hegelund-hansen.dk>
2021-04-28 09:12:43 +0200

    scripts: config tunnel: more routes added

--- scripts/config_tunnel.sh
+++ scripts/config_tunnel.sh
@@ -1,5 +1,5 @@
 # Steen Hegelund
-# Time-Stamp: 2021-Apr-07 17:53
+# Time-Stamp: 2021-Apr-23 11:00
 
 WORKIP=10.205.21.59
 REMOTEIP=$(ssh work ip addr show dev gtun | grep 'link/gre' | sed -Ee 's/^\s+\S+\s+\S+\s+\S+\s+(\S+)/\1/g')
@@ -20,6 +20,9 @@ if [[ -z $GTUN ]]; then
     sudo ip link set dev gtun up
     sudo ip addr add 11.0.0.2/24 dev gtun
     sudo ip route add 1.0.1.0/24 dev gtun
+    sudo ip route add 1.0.2.0/24 dev gtun
+    sudo ip route add 1.0.3.0/24 dev gtun
+    sudo ip route add 1.0.4.0/24 dev gtun
 else
     echo Reuse existing tunnel
 fi
