45273044f7284856c594c895323f65a52a63706e
Steen Hegelund <steen@hegelund-hansen.dk>
2022-01-06 23:22:40 +0100

    fzf: alias with bat preview and neovim edit

--- profile.d/40-alias.sh
+++ profile.d/40-alias.sh
@@ -10,4 +10,4 @@ alias pstl="ps -fH f -u `id -u`"
 alias lsofi="lsof -i -n -P"
 alias sudo='sudo '
 alias du_sorted='du -abx | sort -n | gawk '\''{if(match($0, /^([[:digit:]]+)[\t ]+(.+)/, a)){ s = int(a[1]); n = a[2]; u = "B "; if(s >= 1024){s = s/1024; u = "KB";} if(s >= 1024){s = s/1024; u = "MB";} if(s >= 1024){s = s/1024; u = "GB";} if(s >= 1024){s = s/1024; u = "TB";} printf("%8.2f%s %s\n", s, u, n)}}'\'''
-
+alias fzfp='fzf --preview="bat --style=full --color=always {}" --multi --bind "enter:execute(nvim {})" --bind "space:execute(nvim -O {+})"'
