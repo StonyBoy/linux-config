7d9c8be97223723a759f4ec02c8acb1c0dee4d67
Steen Hegelund <steen@hegelund-hansen.dk>
2022-04-05 20:57:58 +0200

    vim: add Rgf command to search from current file path

--- config/nvim/lua/commands.lua
+++ config/nvim/lua/commands.lua
@@ -1,10 +1,11 @@
 -- Neovim create global commands
 -- Steen Hegelund
--- Time-Stamp: 2022-Jan-31 21:21
+-- Time-Stamp: 2022-Apr-05 20:56
 -- vim: set ts=2 sw=2 sts=2 tw=120 et cc=120 ft=lua :
 
 -- Create Global Ex functions from Lua functions
 vim.cmd([[
 command! TrailspaceTrim lua require('functions').trailspace_trim()
 command! -nargs=* Rg lua require('functions').ripgrep_args(<f-args>)
+command! -nargs=* Rgf lua require('functions').ripgrep_args(vim.fn.expand('%:p:h'), <f-args>)
 ]])
--- config/nvim/lua/functions.lua
+++ config/nvim/lua/functions.lua
@@ -1,6 +1,6 @@
 -- Function Library
 -- Steen Hegelund
--- Time-Stamp: 2022-Mar-31 21:47
+-- Time-Stamp: 2022-Apr-05 20:53
 -- vim: set ts=2 sw=2 sts=2 tw=120 et cc=120 ft=lua :
 
 local Module = {}
@@ -64,7 +64,6 @@ Module.reload_module = function(name)
 end
 
 Module.ls_load = function()
-  -- require('luasnip').cleanup()
   vim.cmd('source ~/.config/nvim/lua/config/snippets.lua')
 end
 
