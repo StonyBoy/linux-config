19a48a7745822f216b2c2e25bcef12bd3e0e9321
Steen Hegelund <steen@hegelund-hansen.dk>
2022-04-19 22:29:37 +0200

    vim: nvim 0.7.0: Create Ex commands

--- config/nvim/lua/commands.lua
+++ config/nvim/lua/commands.lua
@@ -1,11 +1,30 @@
 -- Neovim create global commands
 -- Steen Hegelund
--- Time-Stamp: 2022-Apr-05 20:56
+-- Time-Stamp: 2022-Apr-19 22:27
 -- vim: set ts=2 sw=2 sts=2 tw=120 et cc=120 ft=lua :
 
 -- Create Global Ex functions from Lua functions
-vim.cmd([[
-command! TrailspaceTrim lua require('functions').trailspace_trim()
-command! -nargs=* Rg lua require('functions').ripgrep_args(<f-args>)
-command! -nargs=* Rgf lua require('functions').ripgrep_args(vim.fn.expand('%:p:h'), <f-args>)
-]])
+
+vim.api.nvim_create_user_command('TrailspaceTrim', function()
+  require('functions').trailspace_trim()
+end, {
+  nargs = '*',
+  desc = 'Trim off trailing line whitespace'
+})
+
+vim.api.nvim_create_user_command('Rg', function(opts)
+  require('functions').ripgrep_args(opts.args)
+end, {
+  nargs = '*',
+  complete = 'file',
+  desc = 'Grep command line arguments',
+})
+
+vim.api.nvim_create_user_command('Rgf', function(opts)
+  require('functions').ripgrep_args(vim.fn.expand('%:p:h'), opts.args)
+end, {
+  nargs = '*',
+  complete = 'file',
+  desc = 'Grep command line arguments and use current file path as start folder',
+})
+
