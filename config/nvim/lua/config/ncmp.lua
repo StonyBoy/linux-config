c455b1923f96209f4d346c182f4ce8818d7cc1d9
Steen Hegelund <steen@hegelund-hansen.dk>
2022-04-25 20:24:31 +0200

    vim: updated keymap for nvim complete

--- config/nvim/lua/config/ncmp.lua
+++ config/nvim/lua/config/ncmp.lua
@@ -1,11 +1,40 @@
 -- nvim-cmp configuration
 -- Steen Hegelund
--- Time-Stamp: 2022-Apr-06 22:55
+-- Time-Stamp: 2022-Apr-25 20:22
 -- vim: set ts=2 sw=2 sts=2 tw=120 et cc=120 ft=lua :
 
 print('initialize nvim-cmp')
 
 local cmp = require('cmp')
+
+local sel_next = function(fallback)
+  local luasnip = require('luasnip')
+  if cmp.visible() then
+    cmp.select_next_item()
+  elseif luasnip.expand_or_jumpable() then
+    luasnip.expand_or_jump()
+  else
+    fallback()
+  end
+end
+
+local sel_prev = function(fallback)
+  local luasnip = require('luasnip')
+  if cmp.visible() then
+    cmp.select_prev_item()
+  elseif luasnip.jumpable(-1) then
+    luasnip.jump(-1)
+  else
+    fallback()
+  end
+end
+
+local sel_choice = function()
+  local luasnip = require('luasnip')
+  if luasnip.choice_active() then
+    luasnip.change_choice(1)
+  end
+end
 
 cmp.setup {
   snippet = {
@@ -15,42 +44,19 @@ cmp.setup {
     end,
   },
   mapping = {
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-l>'] = sel_choice,
+    ['<C-n>'] = sel_next,
+    ['<Tab>'] = sel_next,
+    ['<Down>'] = sel_next,
+    ['<S-Tab>'] = sel_prev,
+    ['<C-p>'] = sel_prev,
+    ['<Up>'] = sel_prev,
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
-    ['<C-l>'] = function()
-      local luasnip = require('luasnip')
-      if luasnip.choice_active() then
-        luasnip.change_choice(1)
-      end
-    end,
-    ['<Tab>'] = function(fallback)
-      local luasnip = require('luasnip')
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end,
-    ['<S-Tab>'] = function(fallback)
-      local luasnip = require('luasnip')
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end,
   },
   sources = {
     { name = "buffer" },
