8e9ccea1c710d551cc0701739c6a9a936768db0c
Steen Hegelund <steen@hegelund-hansen.dk>
2022-05-26 19:19:14 +0200

    i3: new i3 config files and new i3 blocks

--- scripts/setup_i3blocks.sh
+++ scripts/setup_i3blocks.sh
@@ -1,6 +1,8 @@
 SRC=~/src
 DST=~/scripts/i3blocks
 mkdir -p $DST
+ln -sf $SRC/i3blocks-contrib/arch-update/arch-update $DST
+ln -sf $SRC/i3blocks-contrib/apt-upgrades/apt-upgrades $DST
 ln -sf $SRC/i3blocks-contrib/calendar/calendar $DST
 ln -sf $SRC/i3blocks-contrib/cpu_usage/cpu_usage $DST
 ln -sf $SRC/i3blocks-contrib/disk/disk $DST
@@ -8,6 +10,7 @@ ln -sf $SRC/i3blocks-contrib/iface/iface $DST
 ln -sf $SRC/i3blocks-contrib/key_layout/key_layout $DST
 ln -sf $SRC/i3blocks-contrib/mediaplayer/mediaplayer $DST
 ln -sf $SRC/i3blocks-contrib/memory/memory $DST
+ln -sf $SRC/i3blocks-contrib/systemctl/systemctl $DST
 ln -sf $SRC/i3blocks-contrib/temperature/temperature $DST
 ln -sf $SRC/i3blocks-contrib/time/time $DST
 ln -sf $SRC/i3blocks-contrib/volume-pulseaudio/volume-pulseaudio $DST
