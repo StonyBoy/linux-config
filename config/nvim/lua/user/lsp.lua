64ef7bf8d18d172421e9ef9b0052ee09ac4e392d
Steen Hegelund <steen@hegelund-hansen.dk>
2022-05-31 11:27:46 +0200

    vim: correct the name of the lspconfig module

--- config/nvim/lua/user/lsp.lua
+++ config/nvim/lua/user/lsp.lua
@@ -1,6 +1,6 @@
 -- Neovim configuration Adding Language Server support
 -- Steen Hegelund
--- Time-Stamp: 2022-May-30 20:52
+-- Time-Stamp: 2022-May-31 11:26
 -- vim: set ts=2 sw=2 sts=2 tw=120 et cc=120 ft=lua :
 
 -- yay ccls
@@ -11,7 +11,7 @@
 -- yay rust-analyzer
 -- yay lua-language-server
 
-local status_ok, nvim_lsp = pcall(require, 'lsp_config')
+local status_ok, nvim_lsp = pcall(require, 'lspconfig')
 if not status_ok then
   return
 end
