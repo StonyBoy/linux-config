aa7b09a7c914ca4f8fdbcc7aa53dde41408791a7
Steen Hegelund <steen@hegelund-hansen.dk>
2022-05-31 09:12:38 +0200

    vim: add autocommands for filetype handling

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,15 +1,16 @@
 -- Neovim configuration
 -- Steen Hegelund
--- Time-Stamp: 2022-May-30 22:00
+-- Time-Stamp: 2022-May-31 09:11
 -- vim: set ts=2 sw=2 sts=2 tw=120 et cc=120 ft=lua :
 
 require('user.options')
 require('user.plugins')
 require('user.colorscheme')
+require('user.lualine')
 require('user.keymaps')
 require('user.commands')
 require('user.functions')
-require('user.lualine')
+require('user.autocommands')
 require('user.gitto')
 require('user.easyalign')
 require('user.bufexplorer')
