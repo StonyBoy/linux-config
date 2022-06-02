42a4ac19cf75eb7d63ae3d8b84418a008afd002d
Steen Hegelund <steen@hegelund-hansen.dk>
2022-06-02 21:52:21 +0200

    vim: updatetime with vimscript embedded

--- config/nvim/lua/user/updatetime.lua
+++ config/nvim/lua/user/updatetime.lua
@@ -1,6 +1,17 @@
 -- Neovim configuration
 -- Steen Hegelund
--- Time-Stamp: 2022-May-30 20:48
+-- Time-Stamp: 2022-Jun-02 21:49
 -- vim: set ts=2 sw=2 sts=2 tw=120 et cc=120 ft=lua :
 
-vim.cmd [[source ~/.config/nvim/timestamp.vim]]
+-- Add name-timestamp header in the beginning of the file
+vim.cmd [[
+function! AddFileHeader()
+  call append(0, ["Steen Hegelund", "Time-Stamp: #", ""])
+  exec '0,3Commentary'
+endfunction
+nnoremap <silent> <Leader><Leader>h :call AddFileHeader()<CR>
+
+let g:update_time_time_stamp_leader = 'Time-Stamp: '
+let g:update_time_time_format = '%Y-%b-%d %H:%M'
+
+]]
