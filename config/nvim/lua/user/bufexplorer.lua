ff91a8fd5cfb3573c9b6e77241afcdc02c375300
Steen Hegelund <steen@hegelund-hansen.dk>
2022-09-27 20:57:36 +0200

    vim: bufexplorer with fullpath default

--- config/nvim/lua/user/bufexplorer.lua
+++ config/nvim/lua/user/bufexplorer.lua
@@ -1,7 +1,7 @@
 -- Neovim configuration
 -- Steen Hegelund
--- Time-Stamp: 2022-May-30 20:47
+-- Time-Stamp: 2022-Sep-27 08:39
 -- vim: set ts=2 sw=2 sts=2 tw=120 et cc=120 ft=lua :
 
 vim.api.nvim_set_keymap('n', '<F8>', '<cmd>ToggleBufExplorer<cr>', { noremap = true, silent = true, })
-
+vim.cmd[[let g:bufExplorerSortBy='fullpath']]
