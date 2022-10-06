874272105b9ff032344e7196057e17a6abbc19a5
Steen Hegelund <steen@hegelund-hansen.dk>
2022-10-06 21:35:13 +0200

    vim: lualine: move edit config to tabline

--- config/nvim/lua/user/lualine.lua
+++ config/nvim/lua/user/lualine.lua
@@ -1,6 +1,6 @@
 -- statusline configuration
 -- Steen Hegelund
--- Time-Stamp: 2022-Oct-06 21:16
+-- Time-Stamp: 2022-Oct-06 21:33
 -- vim: set ts=2 sw=2 sts=2 tw=120 et cc=120 ft=lua :
 
 local status_ok, lualine = pcall(require, 'lualine')
@@ -11,15 +11,12 @@ end
 vim.opt.laststatus = 2
 vim.opt.showmode = false -- Do not show 2nd line edit mode information
 
-local get_branch = require('lualine.components.branch.git_branch').get_branch
-
 -- Show editor configuration: tabs/spaces, tabstop, softtabstop, shiftwidth and textwidth (wrapping)
 local edit_config = function()
   local tabstr = vim.bo.expandtab and 'spc' or 'tab'
   local result = string.format('%s | ts:%d | sw:%d | sts:%d | tw:%d',
     tabstr, vim.bo.tabstop, vim.bo.shiftwidth, vim.bo.softtabstop, vim.bo.textwidth)
-  local margin = vim.api.nvim_win_get_width(0) - string.len(result) - string.len(get_branch()) - 120
-  return margin > 10 and result or tabstr
+  return result
 end
 
 -- Show linenumber and virtual column number (when eg tabs are expanded to spaces)
@@ -31,10 +28,9 @@ end
 local function abbrev_path()
     local ret = vim.fn.expand("%:t") -- Tail part of path (name)
     local fpath = vim.fn.expand('%:p:~') -- Full path optionally using ~ for home
-    local parts = #vim.fn.split(fpath, ':') -- Parts separated by colon
     local margin = vim.api.nvim_win_get_width(0) - string.len(fpath) - 100
 
-    if parts > 1 then
+    if vim.startswith(fpath, 'fugitive') then
         ret = "git:" .. ret
     elseif margin > 10 then
         ret = fpath
@@ -75,7 +71,7 @@ lualine.setup({
     lualine_b = { 'branch', { abbrev_path } },
     lualine_c = {},
     lualine_x = {},
-    lualine_y = { edit_config },
+    lualine_y = {},
     lualine_z = { 'encoding', 'bo:fileformat', 'filetype', file_location, 'progress' },
   },
   inactive_sections = {
@@ -93,7 +89,11 @@ lualine.setup({
         max_length = vim.o.columns,
       },
     },
+    lualine_b = {},
+    lualine_c = {},
     lualine_x = { language_server },
+    lualine_y = { edit_config },
+    lualine_z = {},
   },
   extensions = {},
 })
