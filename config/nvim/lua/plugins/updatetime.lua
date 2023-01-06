8d433674d642dea465332683baaf853cc8631736
Steen Hegelund <steen@hegelund-hansen.dk>
2023-01-06 21:46:32 +0100

    vim: added my own new nvim-update-time plugin

--- config/nvim/lua/plugins/updatetime.lua
+++ config/nvim/lua/plugins/updatetime.lua
@@ -1,28 +1,27 @@
 -- Neovim configuration
 -- Steen Hegelund
--- Time-Stamp: 2023-Jan-03 10:06
+-- Time-Stamp: 2023-Jan-06 21:45
 -- vim: set ts=2 sw=2 sts=2 tw=120 et cc=120 ft=lua :
 
 -- Add name-timestamp header in the beginning of the file
--- The global variables must be set before the plugin is loaded
 
-vim.cmd [[
-  function! AddFileHeader()
-    call append(0, ["Steen Hegelund", "Time-Stamp: #", ""])
-    exec '0,3Commentary'
-  endfunction
-  nnoremap <silent> <Leader><Leader>h :call AddFileHeader()<CR>
+local function add_file_header()
+  vim.cmd [[
+  call append(0, ["Steen Hegelund", "Time-Stamp: 2023-Jan-06 21:45
+  exec '0,3Commentary'
+  ]]
+end
 
-  let g:update_time_time_stamp_leader = 'Time-Stamp: '
-  let g:update_time_time_format = '%Y-%b-%d %H:%M'
-]]
+vim.api.nvim_create_user_command('AddFileHeader', add_file_header, {})
 
 return {
   {
-    'vim-scripts/update-time',
-    lazy = false,
+    'StonyBoy/nvim-update-time',
     dependencies = {
       'tpope/vim-commentary', -- Comment in/out lines of text in various languages
     },
+    config = function()
+      require('nvim-update-time').setup({})
+    end,
   },
 }
