b6107877d516ec2f27bcf95ab996a2368ac3b7c9
Steen Hegelund <steen@hegelund-hansen.dk>
2023-01-10 12:40:56 +0100

    i3: wallpaper and lockscreen handling updated

---
+++ scripts/i3_user_lock.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+# Steen Hegelund
+# Time-Stamp: 2023-Jan-10 11:58
+i3lock --clock --fill --image /opt/wallpapers/lockscreen.jpg --indicator --blur 5 --time-str='%H:%M:%S' --date-str='%A, %Y-%m-%d' --time-color '#ee00eeee' --date-color '#ee00eeee'
+
