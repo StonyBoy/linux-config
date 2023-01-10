87b61892318888497b24502f80aa473adf64b71d
Steen Hegelund <steen@hegelund-hansen.dk>
2023-01-10 14:49:59 +0100

    i3: update user lock script to show date and hostname

--- scripts/i3_user_lock.sh
+++ scripts/i3_user_lock.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 # Steen Hegelund
-# Time-Stamp: 2023-Jan-10 11:58
-i3lock --clock --fill --image /opt/wallpapers/lockscreen.jpg --indicator --blur 5 --time-str='%H:%M:%S' --date-str='%A, %Y-%m-%d' --time-color '#ee00eeee' --date-color '#ee00eeee'
+# Time-Stamp: 2023-Jan-10 13:22
+i3lock --image /opt/wallpapers/lockscreen.jpg --clock --fill --indicator --time-str "" --date-str '%A, %d-%b-%Y' --date-color '#ee00eeee'  --dateoutline-color '#eeee00ee' --dateoutline-width 2 --date-pos "1280:300" --date-size 46 --greeter-size 46 --greeter-text $(hostname) --greeter-color '#ee00eeee' --greeteroutline-color '#eeee00ee' --greeteroutline-width 2
 
