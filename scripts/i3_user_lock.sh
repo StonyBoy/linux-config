2c17f62a435676df8fbf9dfeb10b91f765d51ca4
Steen Hegelund <steen@hegelund-hansen.dk>
2023-01-11 08:49:57 +0100

    i3: assign firefox to workspace 3, update lockscreen with greeter string pos

--- scripts/i3_user_lock.sh
+++ scripts/i3_user_lock.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 # Steen Hegelund
-# Time-Stamp: 2023-Jan-10 13:22
-i3lock --image /opt/wallpapers/lockscreen.jpg --clock --fill --indicator --time-str "" --date-str '%A, %d-%b-%Y' --date-color '#ee00eeee'  --dateoutline-color '#eeee00ee' --dateoutline-width 2 --date-pos "1280:300" --date-size 46 --greeter-size 46 --greeter-text $(hostname) --greeter-color '#ee00eeee' --greeteroutline-color '#eeee00ee' --greeteroutline-width 2
-
+# Time-Stamp: 2023-Jan-11 08:39
+i3lock --image /opt/wallpapers/lockscreen.jpg --clock --fill --indicator --time-str "" --date-str '%A, %d-%b-%Y' --date-color '#ee00eeee'  --dateoutline-color '#eeee00ee' --dateoutline-width 2 --date-pos "1280:300" --date-size 46 --greeter-pos "1280:1100" --greeter-size 46 --greeter-text $(hostname) --greeter-color '#ee00eeee' --greeteroutline-color '#eeee00ee' --greeteroutline-width 2
