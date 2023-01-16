2bf1c8c81e0051064f4e690f29ab36bb704da543
Steen Hegelund <steen@hegelund-hansen.dk>
2023-01-16 21:19:35 +0100

    vim: collect functions and make AddFileHeader independent of Commentary

--- config/nvim/lua/plugins/updatetime.lua
+++ config/nvim/lua/plugins/updatetime.lua
@@ -1,15 +1,21 @@
 -- Neovim configuration
 -- Steen Hegelund
--- Time-Stamp: 2023-Jan-06 22:18
+-- Time-Stamp: 2023-Jan-16 21:11
 -- vim: set ts=2 sw=2 sts=2 tw=120 et cc=120 ft=lua :
 
 -- Add name-timestamp header in the beginning of the file
 
-local function add_file_header()
-  vim.cmd [[
-  call append(0, ["Steen Hegelund", "Time-Stamp: #", ""])
-  exec '0,3Commentary'
-  ]]
+local add_file_header = function()
+  local template = {'Steen Hegelund', 'Time-Stamp: #', require('config.functions').get_modeline()}
+  local lines = {}
+
+  for _, line in ipairs(template) do
+    -- line = ' ' .. line .. ' '
+    line = string.format(vim.opt.commentstring:get(), line)
+    table.insert(lines, line)
+  end
+  table.insert(lines, '')
+  vim.api.nvim_buf_set_text(0, 0, 0, 0, 0, lines)
 end
 
 vim.api.nvim_create_user_command('AddFileHeader', add_file_header, {})
@@ -17,9 +23,6 @@ vim.api.nvim_create_user_command('AddFileHeader', add_file_header, {})
 return {
   {
     'StonyBoy/nvim-update-time',
-    dependencies = {
-      'tpope/vim-commentary', -- Comment in/out lines of text in various languages
-    },
     config = function()
       require('nvim-update-time').setup({ last = 5 })
     end,
