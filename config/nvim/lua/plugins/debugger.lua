b5c728c01b0f5fa8dfee22c08aaa9505c0efb046
Steen Hegelund <steen@hegelund-hansen.dk>
2023-05-10 22:57:23 +0200

    vim: add debug-adapter and ui - added support for rust debugger

---
+++ config/nvim/lua/plugins/debugger.lua
@@ -0,0 +1,95 @@
+-- Neovim configuration
+-- Steen Hegelund
+-- Time-Stamp: 2023-May-10 22:52
+-- vim: set ts=2 sw=2 sts=2 tw=120 et cc=120 ft=lua :
+
+local function keymaps()
+  vim.keymap.set('n', '<leader>c',  require('dap').continue)
+  vim.keymap.set('n', '<leader>o', require('dap').step_over)
+  vim.keymap.set('n', '<leader>i', require('dap').step_into)
+  vim.keymap.set('n', '<leader>r', require('dap').step_out)
+  vim.keymap.set('n', '<leader>b', require('dap').toggle_breakpoint)
+end
+
+-- For rust in Arch Linux you need
+-- lldb for lldb-vscode
+-- llvm for llvm-symbolizer
+local function do_rust()
+  local dap = require('dap')
+  dap.adapters.lldb = {
+    type = 'executable',
+    command = '/usr/bin/lldb-vscode', -- adjust as needed, must be absolute path
+    name = 'lldb'
+  }
+  dap.configurations.rust = {
+    {
+      name = 'Launch',
+      type = 'lldb',
+      request = 'launch',
+      program = function()
+        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+      end,
+      cwd = '${workspaceFolder}',
+      stopOnEntry = false,
+      args = {},
+
+      -- 💀
+      -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+      --
+      --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+      --
+      -- Otherwise you might get the following error:
+      --
+      --    Error on launch: Failed to attach to the target process
+      --
+      -- But you should be aware of the implications:
+      -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+      runInTerminal = true, -- This allows console interaction with the debuggee
+
+      initCommands = function()
+        -- Find out where to look for the pretty printer Python module
+        local rustc_sysroot = vim.fn.trim(vim.fn.system('rustc --print sysroot'))
+
+        local script_import = 'command script import "' .. rustc_sysroot .. '/lib/rustlib/etc/lldb_lookup.py"'
+        local commands_file = rustc_sysroot .. '/lib/rustlib/etc/lldb_commands'
+
+        local commands = {}
+        local file = io.open(commands_file, 'r')
+        if file then
+          for line in file:lines() do
+            table.insert(commands, line)
+          end
+          file:close()
+        end
+        table.insert(commands, 1, script_import)
+
+        return commands
+      end,
+    },
+  }
+
+end
+
+return {
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = {'mfussenegger/nvim-dap'},
+    config = function()
+      local dap = require('dap')
+      local dapui = require('dapui')
+
+      dapui.setup()
+      keymaps()
+      do_rust()
+      dap.listeners.after.event_initialized['dapui_config'] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated['dapui_config'] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited['dapui_config'] = function()
+        dapui.close()
+      end
+    end,
+  },
+}
