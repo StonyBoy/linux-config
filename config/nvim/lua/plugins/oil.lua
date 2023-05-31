9e3847bf7c0f54d5878c677a955498a18a592b35
Steen Hegelund <steen@hegelund-hansen.dk>
2023-05-31 21:03:04 +0200

    vim: oil plugin: configure columns

--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -1,6 +1,6 @@
 -- Neovim configuration
 -- Steen Hegelund
--- Time-Stamp: 2023-May-30 21:32
+-- Time-Stamp: 2023-May-31 21:01
 -- vim: set ts=2 sw=2 sts=2 tw=120 et cc=120 ft=lua :
 
 -- A vim-vinegar like file explorer that lets you edit your filesystem like a normal Neovim buffer.
@@ -11,8 +11,11 @@ return {
       'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     },
     config = function()
-      require("oil").setup()
-      vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
+      local oil = require("oil")
+
+      oil.setup()
+      oil.set_columns({"icon", "size", "permissions", "mtime"})
+
     end,
   }
 }
