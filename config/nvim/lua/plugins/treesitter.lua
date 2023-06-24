e1fba8ad3d7217b24d4379d75f6b067135388ca9
Steen Hegelund <steen@hegelund-hansen.dk>
2023-06-24 13:28:46 +0200

    vim: ensure common treesitter parsers are installed by default

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 -- Neovim configuration
 -- Steen Hegelund
--- Time-Stamp: 2023-Jun-20 21:55
+-- Time-Stamp: 2023-Jun-24 13:28
 -- vim: set ts=2 sw=2 sts=2 tw=120 et cc=120 ft=lua :
 
 -- Highlight, edit, and navigate code using a fast incremental parsing library
@@ -11,6 +11,8 @@ return {
       require 'nvim-treesitter'.setup()
       require 'nvim-treesitter.configs'.setup {
         -- Modules and its options go here
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "yaml", "python", "javascript", "ruby", "devicetree",
+          "bash", "rust", "awk", "cmake", "dockerfile", "cpp", "css", "regex", "yang" },
         highlight = { enable = true },
         incremental_selection = { enable = true },
         textobjects = { enable = true },
