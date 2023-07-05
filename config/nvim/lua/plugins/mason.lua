4578c4e06d8092b01180e2834ae05b8f49836c4f
Steen Hegelund <steen@hegelund-hansen.dk>
2023-07-05 22:43:17 +0200

    vim: use Mason for LSP installation and configuration

---
+++ config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,63 @@
+-- Neovim configuration
+-- Steen Hegelund
+-- Time-Stamp: 2023-Jul-05 22:52
+-- vim: set ts=2 sw=2 sts=2 tw=120 et cc=120 ft=lua :
+
+-- Portable package manager for Neovim that runs everywhere Neovim runs.
+-- Easily install and manage LSP servers, DAP servers, linters, and formatters.
+
+return {
+  {
+    "williamboman/mason.nvim",
+    build = ":MasonUpdate", -- :MasonUpdate updates registry contents
+    config = function()
+      require("mason").setup({
+        ui = {
+          border = "rounded",
+        },
+      })
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup()
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require('lspconfig')
+
+      lspconfig.pylsp.setup({
+        settings = {
+          pylsp = {
+            plugins = {
+              pycodestyle = {
+                ignore = {'W391'},
+                maxLineLength = 120
+              }
+            }
+          }
+        }
+      })
+
+      lspconfig.yamlls.setup({
+        settings = {
+          yaml = {
+            keyOrdering = false
+          }
+        }
+      })
+
+      lspconfig.clangd.setup({})
+      lspconfig.lua_ls.setup({})
+      lspconfig.bashls.setup({})
+      lspconfig.solargraph.setup({})
+      lspconfig.tsserver.setup({})
+      lspconfig.rust_analyzer.setup({})
+      lspconfig.vimls.setup({})
+    end,
+  },
+}
