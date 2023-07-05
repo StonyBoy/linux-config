8b0dfcd9f48008339e55725b160a1eb3a4ff17cc
Steen Hegelund <steen@hegelund-hansen.dk>
2023-07-05 23:00:51 +0200

    vim: mason: ensure used LSPs are installed

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -1,6 +1,6 @@
 -- Neovim configuration
 -- Steen Hegelund
--- Time-Stamp: 2023-Jul-05 22:52
+-- Time-Stamp: 2023-Jul-05 23:00
 -- vim: set ts=2 sw=2 sts=2 tw=120 et cc=120 ft=lua :
 
 -- Portable package manager for Neovim that runs everywhere Neovim runs.
@@ -12,6 +12,9 @@ return {
     build = ":MasonUpdate", -- :MasonUpdate updates registry contents
     config = function()
       require("mason").setup({
+        ensure_installed = {
+          "clangd", "pylsp", "lua_ls", "rust_analyzer", "pylsp", "yamlls", "bashls", "solargraph", "vimls", "tsserver",
+        },
         ui = {
           border = "rounded",
         },
