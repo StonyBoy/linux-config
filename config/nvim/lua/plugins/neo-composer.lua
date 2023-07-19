4f30885fbdb4637194a3c4af181f0db4c26d1740
Steen Hegelund <steen@hegelund-hansen.dk>
2023-07-19 10:32:39 +0200

    vim: neo-composer: use qq as the recording toggle shortcut

--- config/nvim/lua/plugins/neo-composer.lua
+++ config/nvim/lua/plugins/neo-composer.lua
@@ -1,6 +1,6 @@
 -- Neovim configuration
 -- Steen Hegelund
--- Time-Stamp: 2023-Apr-29 18:05
+-- Time-Stamp: 2023-Jul-19 10:32
 -- vim: set ts=2 sw=2 sts=2 tw=120 et cc=120 ft=lua :
 
 
@@ -8,8 +8,12 @@ return {
   {
     "ecthelionvi/NeoComposer.nvim",
     dependencies = { "kkharji/sqlite.lua" },
-    opts = {},
-    config = function(opts)
+    config = function()
+      local opts = {
+        keymaps = {
+          toggle_record = "qq",
+        },
+      }
       require('NeoComposer').setup(opts)
       require('telescope').load_extension('macros')
     end,
