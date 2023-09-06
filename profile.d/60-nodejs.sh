3bc9faa07993e27aaae4953accb02fa5f6428325
Steen Hegelund <steen@hegelund-hansen.dk>
2023-09-06 15:59:46 +0200

    bash: add nodejs nvm config

---
+++ profile.d/60-nodejs.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+# -*-sh-*-
+# Setup nvm for multiple nodejs versions
+
+export NVM_DIR="$HOME/.config/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
