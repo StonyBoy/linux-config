ab12b853bc83fd2d7984f215ead8fed3ca2ccb15
Steen Hegelund <steen@hegelund-hansen.dk>
2023-09-06 15:03:10 +0200

    i3: update for ubuntu 22.04 and new PC

--- scripts/i3_user_lock.sh
+++ scripts/i3_user_lock.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 # Steen Hegelund
-# Time-Stamp: 2023-Jan-25 12:49
-i3lock --image /opt/wallpapers/lockscreen.jpg --clock --fill --indicator --time-str "" --date-str '%A, %d-%b-%Y' --date-color '#0000ffff'  --dateoutline-color '#ffffffff' --dateoutline-width 4 --date-pos "1280:300" --date-size 90 --greeter-pos "1280:1100" --greeter-size 90 --greeter-text $(hostname) --greeter-color '#ff0000ff' --greeteroutline-color '#ffffffff' --greeteroutline-width 4
+# Time-Stamp: 2023-Aug-23 12:59
+~/src/i3lock-color/build/i3lock --image /opt/wallpapers/lockscreen.jpg --clock --fill --indicator --time-str "" --date-str '%A, %d-%b-%Y' --date-color '#0000ffff'  --dateoutline-color '#ffffffff' --dateoutline-width 4 --date-pos "1280:300" --date-size 90 --greeter-pos "1280:1100" --greeter-size 90 --greeter-text $(hostname) --greeter-color '#ff0000ff' --greeteroutline-color '#ffffffff' --greeteroutline-width 4
