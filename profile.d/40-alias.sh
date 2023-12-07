3aa2d802f4f424502260aba43a46f5219eca3023
Steen Hegelund <steen@hegelund-hansen.dk>
2023-12-07 11:27:50 +0100

    bash: remove duplicate alias for du_sorted

--- profile.d/40-alias.sh
+++ profile.d/40-alias.sh
@@ -1,5 +1,3 @@
-alias du_sorted="du -abx | sort -n | gawk '{if(match(\$0, /^([[:digit:]]+)[\\t ]+(.+)/, a)){ s = int(a[1]); n = a[2]; u = \"B \"; if(s >= 1024){s = s/1024; u = \"KB\";} if(s >= 1024){s = s/1024; u = \"MB\";} if(s >= 1024){s = s/1024; u = \"GB\";} if(s >= 1024){s = s/1024; u = \"TB\";} printf(\"%8.2f%s %s\\n\", s, u, n)}}'"
-
 if [ "$(uname)" == "Darwin" ]; then
 	alias ls="ls -G"
 else
