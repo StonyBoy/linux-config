68f25eb1800ad8cc764c2b540fcab52b34cee176
Steen Hegelund <steen@hegelund-hansen.dk>
2024-01-03 21:54:12 +0100

    sway: ubuntu work setup

--- config/waybar/uptime.sh
+++ config/waybar/uptime.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 # Steen Hegelund
-# Time-Stamp: 2023-Feb-04 18:14
+# Time-Stamp: 2024-Jan-03 18:36
 # vim: set ts=4 sw=4 sts=4 tw=120 cc=120 et ft=sh :
 
 while true ; do
-    printf '{"text": "%s", "class": "custom-uptime", "tooltip": "Computer uptime in Days>Hours>Minutes"}\n' $(strict_uptime)
+    printf '{"text": "%s", "class": "custom-uptime", "tooltip": "Computer uptime in Days>Hours>Minutes"}\n' $(~/scripts/strict_uptime)
     sleep 60
 done
