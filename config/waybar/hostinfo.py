d6be9be0979f685a33c22da11e53e53f296e51eb
Steen Hegelund <steen@hegelund-hansen.dk>
2024-01-06 21:17:41 +0100

    sway: waybar: hostinfo element replacing hostname and uptime

---
+++ config/waybar/hostinfo.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import argparse
+import os
+import subprocess
+import time
+import json
+
+SECS_PER_DAY = 86400
+SECS_PER_HOUR = 3600
+SECS_PER_MINUTE = 60
+
+def show_host_info(uname, secs):
+    sep = chr(0xf104)
+    days = secs // SECS_PER_DAY
+    remainder = secs % SECS_PER_DAY
+    hours = remainder // SECS_PER_HOUR
+    minutes = remainder % SECS_PER_HOUR // SECS_PER_MINUTE
+
+    text = f"<b>{uname.nodename}</b>: {days}{sep}{hours:>02}{sep}{minutes:>02}"
+    tooltip = f"Host Information:\n  Hostname: {uname.nodename}\n  Release: {uname.release}\n  Uptime: {days} days {hours} hours {minutes} minutes"
+    res = {"text": text, "tooltip": tooltip, "class": "custom-hostinfo", "percentage": 100}
+
+    print(json.dumps(res))
+
+
+def get_host_info():
+    uname = os.uname()
+    show_host_info(os.uname(), int(time.clock_gettime(time.CLOCK_BOOTTIME)))
+
+
+def main():
+    get_host_info()
+
+
+if __name__ == '__main__':
+    main()
