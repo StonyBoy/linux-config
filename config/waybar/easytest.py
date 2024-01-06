ba8dcc02258cbd63ff73729a503d237db396f0b3
Steen Hegelund <steen@hegelund-hansen.dk>
2024-01-06 20:24:44 +0100

    sway: waybar: added custom easytest element and script

---
+++ config/waybar/easytest.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+import argparse
+import subprocess
+import json
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('user')
+    parser.add_argument('-p', '--path', default='/usr/local/bin/et')
+    parser.add_argument('-s', '--server', default='dk')
+
+    return parser.parse_args()
+
+
+def show_easytest(elem):
+    text = f"{elem['name']}"
+    tooltip = f"Easytest Status:\n  Server: {elem['name']}\n  Target: {elem['vsc']}\n  User: {elem['user']}\n  Since: {elem['date']} {elem['time']}"
+    res = {"text": text, "tooltip": tooltip, "class": "custom-easytest", "percentage": 100}
+    print(json.dumps(res))
+
+
+def show_empty():
+    res = {"text": "", "tooltip": "", "class": "", "percentage": 0}
+    print(json.dumps(res))
+
+
+def get_easytest_status(args):
+    found = False
+    cp = subprocess.run([args.path, 'status', args.server], capture_output=True)
+    if cp.returncode == 0:
+        lines = cp.stdout.decode().split('\n')
+        names = ['name', 'server', 'status', 'pcb', 'loop_port', 'loop_1pps', 'vsc', 'family', 'user', 'date', 'time']
+        for line in lines[2:]:
+            values = line.split()
+            if len(values) > 2:
+                elem = dict(zip(names, values))
+                if elem['status'] == 'busy' and elem['user'] == args.user:
+                    show_easytest(elem)
+                    found = True
+    if not found:
+        show_empty()
+
+
+def main():
+    arguments = parse_arguments()
+    get_easytest_status(arguments)
+
+
+if __name__ == '__main__':
+    main()
