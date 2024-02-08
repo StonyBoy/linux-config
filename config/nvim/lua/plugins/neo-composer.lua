95c966240341ae1f47ce298a0ab3ebb56f5a64e0
Steen Hegelund <steen@hegelund-hansen.dk>
2024-02-08 15:28:26 +0100

    vim: neo-composer: popup window colors to match solarized8

--- config/nvim/lua/plugins/neo-composer.lua
+++ config/nvim/lua/plugins/neo-composer.lua
@@ -1,6 +1,6 @@
 -- Neovim configuration
 -- Steen Hegelund
--- Time-Stamp: 2023-Jul-19 10:32
+-- Time-Stamp: 2024-Feb-01 10:11
 -- vim: set ts=2 sw=2 sts=2 tw=120 et cc=120 ft=lua :
 
 
@@ -13,6 +13,13 @@ return {
         keymaps = {
           toggle_record = "qq",
         },
+        colors = {
+          bg = "#fdf6e3",
+          fg = "#839496",
+          red = "#ec5f67",
+          blue = "#5fb3b3",
+          green = "#99c794",
+        }
       }
       require('NeoComposer').setup(opts)
       require('telescope').load_extension('macros')
