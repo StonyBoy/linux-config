6cfebf25946299f5634c872419e71360f845a91f
Steen Hegelund <steen@hegelund-hansen.dk>
2024-02-11 20:17:23 +0100

    vim: change mini.surround plugin mappings

--- config/nvim/lua/plugins/mini-surround.lua
+++ config/nvim/lua/plugins/mini-surround.lua
@@ -1,15 +1,28 @@
 -- Neovim configuration
 -- Steen Hegelund
--- Time-Stamp: 2024-Feb-11 17:23
+-- Time-Stamp: 2024-Feb-11 20:16
 -- vim: set ts=2 sw=2 sts=2 tw=120 et cc=120 ft=lua :
 
 return {
   {
     "echasnovski/mini.surround",
     event = "VeryLazy",
-    opts = {},
     config = function()
-      require('mini.surround').setup()
+      local opts = {
+        mappings = {
+          add = 'gsa', -- Add surrounding in Normal and Visual modes
+          delete = 'gsd', -- Delete surrounding
+          find = 'gsf', -- Find surrounding (to the right)
+          find_left = 'gsF', -- Find surrounding (to the left)
+          highlight = 'gsh', -- Highlight surrounding
+          replace = 'gsr', -- Replace surrounding
+          update_n_lines = 'gsn', -- Update `n_lines`
+
+          suffix_last = 'l', -- Suffix to search with "prev" method
+          suffix_next = 'n', -- Suffix to search with "next" method
+        },
+      }
+      require('mini.surround').setup(opts)
     end,
   }
 }
