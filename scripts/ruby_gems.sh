c720d91a9876e5b68f423d9b0008519a44c0d3b2
Steen Hegelund <steen@hegelund-hansen.dk>
2024-02-16 10:55:52 +0100

    scripts: ruby gem installation

---
+++ scripts/ruby_gems.sh
@@ -0,0 +1,21 @@
+# Steen Hegelund
+# Time-Stamp: 2024-Feb-16 08:44
+# vim: set ts=4 sw=4 sts=4 tw=120 cc=120 et ft=sh :
+
+# rvm install 3.2.3
+
+gem install debug
+gem install solargraph
+gem install digest-crc # crc32
+gem install date
+gem install erb
+gem install fileutils
+gem install json
+gem install logger
+gem install net/http
+gem install nokogiri
+gem install open3
+gem install optparse
+gem install uri
+gem install yaml
+gem install neovim
