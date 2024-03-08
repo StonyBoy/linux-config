d510e745ec83346ed8afc136fe36eb1ad262408f
Steen Hegelund <steen@hegelund-hansen.dk>
2024-03-08 14:38:35 +0100

    bash: remove old work environment

--- profile.d/30-work-env.sh
+++
@@ -1,9 +0,0 @@
-[ -d /opt/coverity/cov-analysis-linux64-2017.07-SP1/bin/ ] && pathappend /opt/coverity/cov-analysis-linux64-2017.07-SP1/bin/
-
-[ -d /opt/fsl-qoriq-glibc-x86_64-aarch64-toolchain-2.0/sysroots/x86_64-fslsdk-linux/usr/bin/aarch64-fsl-linux/ ] && pathappend /opt/fsl-qoriq-glibc-x86_64-aarch64-toolchain-2.0/sysroots/x86_64-fslsdk-linux/usr/bin/aarch64-fsl-linux/
-
-alias arm64make='make -j12 ARCH=arm64 CROSS_COMPILE=aarch64-fsl-linux- '
-alias make-mipsel-uboot='make -j12 KBUILD_OUTPUT=./build ARCH=mipsel CROSS_COMPILE=/opt/mscc/mscc-brsdk-mips-2018.02-002/stage2/smb/x86_64-linux/bin/mipsel-linux-'
-
-
-export ARMLMD_LICENSE_FILE=27003@bby1snlic05.pmc-sierra.bc.ca
