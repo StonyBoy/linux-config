3f2cc12232d2cbf2270b17b49877a9630db0a6bd
Steen Hegelund <steen@hegelund-hansen.dk>
2024-06-06 08:55:47 +0200

    vim: which-key: set a longer timeout

--- config/nvim/lua/plugins/which-key.lua
+++ config/nvim/lua/plugins/which-key.lua
@@ -1,6 +1,6 @@
 -- Neovim configuration
 -- Steen Hegelund
--- Time-Stamp: 2024-Feb-11 16:26
+-- Time-Stamp: 2024-Jun-03 10:05
 -- vim: set ts=2 sw=2 sts=2 tw=120 et cc=120 ft=lua :
 
 -- Displays a popup with possible key bindings of the command you started typing
@@ -9,7 +9,7 @@ return {
   event = "VeryLazy",
   init = function()
     vim.o.timeout = true
-    vim.o.timeoutlen = 500
+    vim.o.timeoutlen = 1000
   end,
   opts = {
     -- your configuration comes here
