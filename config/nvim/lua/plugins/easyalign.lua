911e9e3920f7a24f83d7a706945e5e9f957f1621
Steen Hegelund <steen@hegelund-hansen.dk>
2025-01-20 16:46:56 +0100

    vim: easyalign that uses selection modes

--- config/nvim/lua/plugins/easyalign.lua
+++ config/nvim/lua/plugins/easyalign.lua
@@ -1,6 +1,6 @@
 -- Neovim configuration
 -- Steen Hegelund
--- Time-Stamp: 2022-May-30 20:45
+-- Time-Stamp: 2025-Jan-20 16:38
 -- vim: set ts=2 sw=2 sts=2 tw=120 et cc=120 ft=lua :
 
 return {
@@ -8,7 +8,8 @@ return {
     'junegunn/vim-easy-align',                     -- Align text on specific characters in nice columns
     config = function()
       vim.api.nvim_set_keymap('n', 'ga', '<cmd>EasyAlign<cr>', { noremap = true, silent = true, }) -- Start interactive EasyAlign for a motion/text object (e.g. gaip)
-      vim.api.nvim_set_keymap('x', 'ga', '<cmd>EasyAlign<cr>', { noremap = true, silent = true, }) -- Start interactive EasyAlign in visual mode (e.g. vipga)
+      vim.api.nvim_set_keymap('v', 'ga', '<Plug>(EasyAlign)', {}) -- Start interactive EasyAlign in visual mode (e.g. vipga), Visual and Select mode
+      vim.api.nvim_set_keymap('x', 'ga', '<Plug>(EasyAlign)', {}) -- Start interactive EasyAlign in visual mode (e.g. vipga), Visual mode
     end,
   }
 }
