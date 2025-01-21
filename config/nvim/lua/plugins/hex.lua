9c943283015e9e25080ad2b4930d13177072162b
Steen Hegelund <steen@hegelund-hansen.dk>
2025-01-21 17:04:00 +0100

    vim: adding own hex plugin fork

--- config/nvim/lua/plugins/hex.lua
+++ config/nvim/lua/plugins/hex.lua
@@ -1,12 +1,12 @@
 -- Neovim configuration
 -- Steen Hegelund
--- Time-Stamp: 2023-May-10 10:06
+-- Time-Stamp: 2025-Jan-21 17:02
 -- vim: set ts=2 sw=2 sts=2 tw=120 et cc=120 ft=lua :
 
 
 return {
   {
-    'RaafatTurki/hex.nvim',                        -- Hex editing done right.  Uses xxd utility, so it must be in $PATH
+    'StonyBoy/hex.nvim',                        -- Hex editing done right.  Uses xxd utility, so it must be in $PATH
     config = function()
       require('hex').setup()
     end,
