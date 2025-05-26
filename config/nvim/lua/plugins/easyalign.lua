6217ecb29ca7af45aa8d678d9054fe41eec0dbf0
Steen Hegelund <steen@hegelund-hansen.dk>
2025-05-26 15:32:57 +0200

    nvim: easy-align: add user defined alignment operators

--- config/nvim/lua/plugins/easyalign.lua
+++ config/nvim/lua/plugins/easyalign.lua
@@ -1,6 +1,6 @@
 -- Neovim configuration
 -- Steen Hegelund
--- Time-Stamp: 2025-Jan-21 09:50
+-- Time-Stamp: 2025-May-26 15:31
 -- vim: set ts=2 sw=2 sts=2 tw=120 et cc=120 ft=lua :
 
 return {
@@ -10,6 +10,35 @@ return {
       vim.api.nvim_set_keymap('n', 'ga', '<Plug>(EasyAlign)', {}) -- Start interactive EasyAlign for a motion/text object (e.g. gaip)
       vim.api.nvim_set_keymap('v', 'ga', '<Plug>(EasyAlign)', {}) -- Start interactive EasyAlign in visual mode (e.g. vipga), Visual and Select mode
       vim.api.nvim_set_keymap('x', 'ga', '<Plug>(EasyAlign)', {}) -- Start interactive EasyAlign in visual mode (e.g. vipga), Visual mode
+
+      -- User defined alignment
+      vim.g.easy_align_delimiters = {
+        ['>'] = {
+          pattern = '>>\\|=>\\|>'
+        },
+        ['/'] = {
+          pattern = '//\\+\\|/\\*\\|\\*/',
+          delimiter_align = 'l',
+        },
+        [']'] = {
+          pattern = '[[\\]]',
+          left_margin = 0,
+          right_margin = 0,
+          stick_to_left = 0
+        },
+        [')'] = {
+          pattern = '[()]',
+          left_margin = 0,
+          right_margin = 0,
+          stick_to_left = 0
+        },
+        ['d'] = {
+          pattern = ' \\(\\S\\+\\s*[;=]\\)\\@=',
+          left_margin = 0,
+          right_margin = 0,
+          stick_to_left = 0
+        }
+      }
     end,
   }
 }
