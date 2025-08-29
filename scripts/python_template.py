bbe2806e7bd4a21ed439d3f1154f39938f5b56a7
Steen Hegelund <steen@hegelund-hansen.dk>
2025-08-29 10:46:35 +0200

    Adding the python script template file

---
+++ scripts/python_template.py
@@ -0,0 +1,33 @@
+#!/usr/local/pyvenv/steen/bin/python3
+
+# Steen Hegelund
+# Time-Stamp: 2025-Aug-29 10:44
+# vim: set ts=4 sw=4 sts=4 tw=120 cc=120 et ft=python :
+'''
+Description
+'''
+
+import argparse
+import sys
+
+
+def parse_arguments() -> (argparse.ArgumentParser, argparse.Namespace):
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawTextHelpFormatter, exit_on_error=False)
+
+    parser.add_argument('--verbose', action='store_true', help='Provide more information')
+
+    try:
+        return parser, parser.parse_args()
+    except argparse.ArgumentError:
+        parser.print_help()
+        sys.exit(1)
+
+
+def main():
+    parser, args = parse_arguments()
+    if len(vars(args)) == 1:
+        parser.print_help()
+
+
+if __name__ == '__main__':
+    main()
