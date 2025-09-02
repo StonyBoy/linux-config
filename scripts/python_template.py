d0e8be371f142249b7c40f0775df936a83dcf1de
Steen Hegelund <steen@hegelund-hansen.dk>
2025-09-02 09:44:14 +0200

    python_template: simple invocation - no need for venv

--- scripts/python_template.py
+++ scripts/python_template.py
@@ -1,7 +1,7 @@
-#!/usr/local/pyvenv/steen/bin/python3
+#!/usr/bin/env python3
 
 # Steen Hegelund
-# Time-Stamp: 2025-Aug-29 10:44
+# Time-Stamp: 2025-Sep-02 09:42
 # vim: set ts=4 sw=4 sts=4 tw=120 cc=120 et ft=python :
 '''
 Description
