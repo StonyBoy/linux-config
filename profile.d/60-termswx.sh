e9d9f3c9d60654ec978c7d9b2c52e0f162989cc1
Steen Hegelund <steen@hegelund-hansen.dk>
2026-06-04 14:16:11 +0200

    termswx: add tab expansion for serial configuration names

---
+++ profile.d/60-termswx.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+#  vim: set ts=4 sw=4 sts=4 tw=120 cc=120 et ft=sh :
+# Steen Hegelund
+# Time-Stamp: 2026-May-18 10:35
+# Adding connection name completion for termswx
+
+source <(termswx --bpaf-complete-style-bash)
